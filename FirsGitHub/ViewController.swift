7684df0c8c5e46512e6a80ea55148be1f8ea3fed
OmurbekovTemirlan <Japarhanovaldik@gmail.com>
2024-02-02 22:07:04 +0600

    Initial Commit

---
+++ FirsGitHub/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FirsGitHub
+//
+//  Created by Apple on 2.2.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
